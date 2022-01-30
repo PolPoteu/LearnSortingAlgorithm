e63fb8c3be25d10d2f8fd9ad73226c48ed321191
PolPoteu <ratattuja@gmail.com>
2022-01-30 16:25:41 +0100

    feat: add radix sort algo

--- sorting-app/src/app/app.module.ts
+++ sorting-app/src/app/app.module.ts
@@ -51,6 +51,7 @@ import { NavigationComponent } from './navigation/navigation.component';
     RouterModule.forRoot([
       { path: 'bubble-sort', component: BubblesortComponent },
       { path: 'heap-sort', component: HeapsortComponent },
+      { path: 'radix-sort', component: RadixsortComponent },
       { path: 'counting-sort', component: CountingsortComponent },
       { path: 'selection-sort', component: SelectionsortComponent },
       { path: 'insertion-sort', component: InsertionsortComponent },
